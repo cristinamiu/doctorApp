b6a1403a7b94477c31c1f210a8644f3f1d57d39f
Cristina <cristinamaria.miulescu@gmail.com>
2022-05-14 14:22:55 +0300

    Initial commit: Setup server and client

---
+++ server/index.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(process.env.PORT, () => {
+  console.log("Server listening on port " + process.env.PORT);
+});
