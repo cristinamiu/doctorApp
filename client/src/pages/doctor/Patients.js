631edf30664acdb65d6ab4566c68b89a2811a8f3
Cristina <cristinamaria.miulescu@gmail.com>
2022-05-16 23:14:35 +0300

    Temporary commit

--- client/src/pages/doctor/Patients.js
+++ client/src/pages/doctor/Patients.js
@@ -2,8 +2,23 @@ import React, { useState, useContext, useEffect } from "react";
 import DoctorSidebar from "./Sidebar";
 import axios from "axios";
 import Jumbotron from "./Jumbotron";
+import { AuthContext } from "../../context/AuthContext";
 
 function Patients() {
+  const { authState } = useContext(AuthContext);
+  const [patients, setPatients] = useState([]);
+  const [fName, setfName] = useState("");
+  const [lName, setlName] = useState("");
+  const doctorId = authState.secondId;
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/doctors/${doctorId}/my-appointments`)
+      .then((response) => {
+        console.log(response.data);
+        setPatients(response.data);
+      });
+  }, []);
   return (
     <div className="container-fluid d-flex flex-column p-0">
       <div class="row">
@@ -13,75 +28,47 @@ function Patients() {
           <div>
             <Jumbotron title="My Patients" />
           </div>
-          <div className="d-flex flex-row-reverse">
-            <button
-              type="button"
-              class="btn btn-success mb-4"
-              data-bs-toggle="modal"
-              data-bs-target="#exampleModal"
-            >
-              Add doctor
-            </button>
-          </div>
 
-          <table class="table align-middle mb-0 bg-white">
+          <table class="table align-middle mb-0 bg-white mt-5">
             <thead class="bg-light">
               <tr>
                 <th>#</th>
                 <th>Id</th>
                 <th>Name</th>
-                <th>Department</th>
                 <th>Actions</th>
               </tr>
             </thead>
             <tbody>
-              {/* {doctors.map((doctor, key) => ( */}
-              <tr>
-                <td>
-                  <img
-                    src="/images/doctor.png"
-                    className="rouded-circle justify-content-start"
-                    alt=""
-                  />
-                </td>
-                {/* <td>{doctor.id}</td> */}
-                <td>
-                  <div class=" align-items-center">
-                    <div>
-                      {/* <p class="fw-bold mb-1">{doctor.name}</p> */}
-                      {/* <p class="text-muted mb-0">{doctor.email}</p> */}
+              {patients.map((patient, key) => (
+                <tr>
+                  <td>
+                    <img
+                      src="/images/doctor.png"
+                      className="rouded-circle justify-content-start"
+                      alt=""
+                    />
+                  </td>
+                  <td>{patient.Patient.id}</td>
+                  <td>
+                    <div class=" align-items-center">
+                      <div>
+                        <p class="fw-bold mb-1">{patient.Patient.name}</p>
+                      </div>
                     </div>
-                  </div>
-                </td>
-                <td>
-                  <div class=" align-items-center">
-                    <div>
-                      {/* <p class="">{doctor.Doctors[0].department}</p> */}
+                  </td>
+                  <td>
+                    <div class="btn-group m-2">
+                      <a
+                        type="button"
+                        class="btn btn-primary btn-sm"
+                        href={`/doctors/my-patients/${patient.Patient.id}`}
+                      >
+                        View
+                      </a>
                     </div>
-                  </div>
-                </td>
-                <td>
-                  <div class="btn-group m-2">
-                    <a
-                      type="button"
-                      class="btn btn-primary btn-sm"
-                      // href={`/doctors/${doctor.id}`}
-                    >
-                      View
-                    </a>
-                  </div>
-                  <div class="btn-group">
-                    <button
-                      type="button"
-                      class="btn btn-danger btn-sm"
-                      // onClick={() => deleteDoctor(doctor.id)}
-                    >
-                      Delete
-                    </button>
-                  </div>
-                </td>
-              </tr>
-              {/* ))} */}
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
