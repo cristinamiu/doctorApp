4cad9d5a92224bb583b48d19a91e5cf0b0c87746
Cristina <cristinamaria.miulescu@gmail.com>
2022-05-17 11:09:28 +0300

    Adds layout dashboard

--- client/src/pages/doctor/Dashboard.js
+++ client/src/pages/doctor/Dashboard.js
@@ -1,14 +1,19 @@
 import React, { useContext } from "react";
 import DoctorSidebar from "./Sidebar";
 import { AuthContext } from "../../context/AuthContext";
-
+import Jumbotron from "./Jumbotron";
 function Dashboard() {
   const { authState } = useContext(AuthContext);
   return (
-    <div className="container-fluid ">
-      <div class="row vh-100">
+    <div className="container-fluid d-flex flex-column p-0">
+      <div class="row">
         <DoctorSidebar />
-        <div class="col-sm p-0">{authState.secondId}</div>
+
+        <div class="col">
+          <div>
+            <Jumbotron title="Dashboard" />
+          </div>
+        </div>
       </div>
     </div>
   );
